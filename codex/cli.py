9a668fd1c83e0ad1dcb0fe232650966a8de8eea8
Omar Ashour <omar.ashour22@gmail.com>
2023-06-07 13:19:20 -0700

    Basic CLI implementation with web server

--- codex/cli.py
+++ codex/cli.py
@@ -10,6 +10,9 @@ import sys
 import os
 from inspect import cleandoc
 import datetime
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import webbrowser
+from threading import Timer
 
 from codex import Codex
 
@@ -23,6 +26,22 @@ __maintainer__ = metadata["Maintainer"]
 __maintainer_email__ = metadata["Maintainer-email"]
 __summary__ = metadata["Summary"]
 __URL__ = metadata["Home-page"]
+
+def _open_browser(port):
+    webbrowser.open(f"http://localhost:{port}/")
+
+def _split_qe_and_vasp_files(filenames):
+    files_qe = [f for f in filenames if f.endswith(".in") or f.endswith(".pwi")]
+    files_vasp = [
+        f
+        for f in filenames
+        if f.startswith("INCAR")
+        or f.startswith("KPOINTS")
+        or f.startswith("POSCAR")
+        or f.startswith("POTCAR")
+    ]
+
+    return files_qe, files_vasp
 
 
 def _get_parser():
@@ -63,10 +82,34 @@ def _get_parser():
         help="Database version to use (default: latest)",
     )
 
+    parser.add_argument(
+        "--version",
+        action="store_true",
+        help="Return the version of DFT-CODEX",
+    )
+
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        action="store_true",
+        help="Enable verbose output",
+    )
+
+    parser.add_argument(
+        "--port",
+        "-p",
+        default="42069",
+        metavar="P",
+        help="Port to start HTTP server on (default: 42069)",
+    )
+
     return parser
 
 
 def main():
+    """
+    The main CLI for codex
+    """
     args = _get_parser().parse_args()
     logging.basicConfig(
         filename="codex.log",
@@ -79,6 +122,10 @@ def main():
     logging.info(" ".join(sys.argv[:]))
     logging.getLogger("").addHandler(console)
 
+    if args.verbose:
+        console.setLevel(logging.DEBUG)
+    if args.version:
+        sys.exit(f"DFT-CODEX version {__version__}")
     if args.filenames is not None and args.work_dir is not None:
         sys.exit("Cannot specify filenames and directory at the same time.")
 
@@ -87,33 +134,18 @@ def main():
         work_dir = os.getcwd()
 
     if args.filenames is None:
-        filenames = os.listdir(work_dir)
-        files_qe = [f for f in filenames if f.endswith(".in") or f.endswith(".pwi")]
-        files_vasp = [
-            f
-            for f in filenames
-            if f.startswith("INCAR")
-            or f.startswith("KPOINTS")
-            or f.startswith("POSCAR")
-            or f.startswith("POTCAR")
-        ]
+        files_in_dir = os.listdir(work_dir)
+        files_qe, files_vasp = _split_qe_and_vasp_files(files_in_dir)
         filenames = files_qe + files_vasp
         if not filenames:
-            sys.exit("No input files found in directory.") 
+            sys.exit("No input files found in directory.")
         else:
             logging.info(f"Detected {len(filenames)} files in {work_dir}.")
     else:
         filenames = args.filenames
-        files_qe = [f for f in filenames if f.endswith(".in") or f.endswith(".pwi")]
-        files_vasp = [
-            f
-            for f in filenames
-            if f.startswith("INCAR")
-            or f.startswith("KPOINTS")
-            or f.startswith("POSCAR")
-            or f.startswith("POTCAR")
-        ]
-    # Whether to use QE and/or VASP or not
+        files_qe, files_vasp = _split_qe_and_vasp_files(filenames)
+
+    # Whether to use QE and/or VASP
     use_qe = bool(files_qe)
     use_vasp = bool(files_vasp)
 
@@ -122,10 +154,10 @@ def main():
     database_dir = resources.files("codex.database")
 
     # If database version is "latest", check which one it is
-    db_files = resources.contents("codex.database")
+    db_contents = resources.contents("codex.database")
     if args.dbversion == "latest":
-        qe_dbs= [float(f.split("-")[1]) for f in db_files if f.startswith("qe-")]
-        vasp_dbs = [float(f.split("-")[1]) for f in db_files if f.startswith("vasp-")]
+        qe_dbs = [float(f.split("-")[1]) for f in db_contents if f.startswith("qe-")]
+        vasp_dbs = [float(f.split("-")[1]) for f in db_contents if f.startswith("vasp-")]
         qe_version = None
         vasp_version = None
         if qe_dbs:
@@ -140,16 +172,37 @@ def main():
         qe_version = args.dbversion
         vasp_version = args.dbversion
         # Check if they exist
-        if f"qe-{qe_version}" not in db_files:
+        if f"qe-{qe_version}" not in db_contents:
             sys.exit(f"QE database version {qe_version} not found.")
-        if f"vasp-{vasp_version}" not in db_files:
+        if f"vasp-{vasp_version}" not in db_contents:
             sys.exit(f"VASP database version {vasp_version} not found.")
 
     filenames = filenames[0]  # TODO: add option for multiple files...
+    codex = None
     if use_qe:
-        Codex(filenames, database_dir, qe_version)
+        codex = Codex(files_qe, database_dir, qe_version)
+        html_filename = "index.html" if not use_vasp else "index_qe.html"
+        codex.build(html_filename)
     if use_vasp:
         sys.exit("VASP not implemented yet.")
+        codex = Codex(files_vasp, database_dir, vasp_version)
+        html_filename = "index.html" if not use_qe else "index_vasp.html"
+        codex.build(html_filename)
+    if codex is None:
+        # TODO: better error handling
+        sys.exit("No input files generated for some reason...")
+    if use_qe and use_vasp:
+        # TODO: generate index file with links to both QE and VASP codexes
+        sys.exit("QE+VASP not implemented yet.")
+
+    # TODO: directory handling needs to be better
+    # More consistency with Codex.build()
+    os.chdir(os.path.join(work_dir, '.codex'))
+    port = int(args.port)
+    httpd = HTTPServer(("localhost", port), SimpleHTTPRequestHandler)
+    logging.info(f"Serving HTTP on localhost port {port} (http://localhost:{port}/)...")
+    Timer(1, _open_browser, [port]).start()
+    httpd.serve_forever()
 
 if __name__ == "__main__":
     main()
