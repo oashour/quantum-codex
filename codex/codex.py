8194ab00cd53505dc25330e54095b8ec354353f7
Omar Ashour <omar.ashour22@gmail.com>
2023-06-08 01:14:37 -0700

    Support for all QE packages

--- codex/codex.py
+++ codex/codex.py
@@ -11,6 +11,7 @@ import shutil
 from string import Template
 from inspect import cleandoc
 from importlib import resources
+import logging
 
 import f90nml
 
@@ -18,12 +19,11 @@ import f90nml
 def _nl_print(string):
     if string is True:
         return ".true."
-    elif string is False:
+    if string is False:
         return ".false."
-    elif isinstance(string, str):
+    if isinstance(string, str):
         return f"'{string}'"
-    else:
-        return string
+    return string
 
 
 # TODO: use local jquery
@@ -57,24 +57,14 @@ class Codex:
 
         # TODO: need some switch for determining whether QE or VASP
         self.code = "qe"  # Need better terminology
-        # TODO: need to fix this to determine pacakge from input files
-        # Can read all namelists and match them against the database and see which dictionary they all belong to simultaneously
-        # If there are multiple options, compare the names of the tags as well
-        #def find_package(data, partial_match):
-        #    for top_key, sub_dict in data.items():
-        #        if all(k in sub_dict and sub_dict[k] == v for k, v in partial_match.items()):
-        #            return top_key
-        #    return None
 
-        self.package = ["pw"] * len(input_filenames)
         self.database_dir = os.path.join(self.base_db_dir, f"{self.code}-{dbversion}")
         self.database_filename = os.path.join(self.database_dir, "database.json")
 
         # Load Database
         with open(self.database_filename) as f:
             self._database = json.load(f)
-        #package = find_package(self._database, f90nml.read(input_filenames[0]))
-        #return package
+        self.packages = self.get_packages(input_filenames)
 
         body_html = []
         if self.code == "qe":
@@ -101,22 +91,50 @@ class Codex:
         with open(path, "w") as f:
             f.write(self.html)
 
+    # TODO: abstract to VASP
+    def get_packages(self, filenames):
+        """
+        This function finds what package a QE input file is for.
+        Given the database, it flattens the dictionaries and
+        the namelists and returns the list of packages that match a given file.
+        """
+        database = self._database
+
+        flat_db = self._flatten_dict(database)
+        packages = []
+        for file in filenames:
+            input_data = f90nml.read(file)
+            flat_input = self._flatten_dict({"_": input_data})["_"]
+            matches = []
+            for package, mashed_tags in flat_db.items():
+                if all(tag in mashed_tags for tag in flat_input):
+                    matches.append(package)
+            if len(matches) == 0:
+                raise ValueError(f"Could not find package for {file}")
+            if len(matches) > 1:
+                logging.warning(f"Found multiple possible packages for {file}: {matches}.")
+                logging.warning("Using first package.")
+            packages.append(matches[0])
+
+        return packages
+
+
     def _generate_dir_tree(self):
         """
         Copies appropriate files from the database to the working directory
         and creates the tree
         """
-        if not os.path.exists(self.working_dir):
-            os.mkdir(self.working_dir)
+        if os.path.exists(self.working_dir):
+            shutil.rmtree(self.working_dir)
+        os.mkdir(self.working_dir)
         tags_dir = os.path.join(self.working_dir, f"tags-{self.code}")
-        if not os.path.exists(tags_dir):
-            os.mkdir(tags_dir)
+        os.mkdir(tags_dir)
 
         style_css = os.path.join(self.base_db_dir, "style.css")
         tags_css = os.path.join(self.base_db_dir, f"tag-{self.code}.css")
         script_js = os.path.join(self.base_db_dir, "script.js")
         docs_html = []
-        for p in self.package:
+        for p in self.packages:
             docs_html.append(os.path.join(self.database_dir, f"{p}.html"))
 
         shutil.copy2(style_css, self.working_dir)
@@ -167,7 +185,7 @@ class Codex:
     def _generate_tag_webpages(self):
         database = self._database
         tags_dir = os.path.join(self.working_dir, "tags-qe")
-        packages = set(self.package)
+        packages = set(self.packages)
         for p in packages:
             os.mkdir(os.path.join(tags_dir, p))
             for nl in database[p].keys():
@@ -240,7 +258,7 @@ class Codex:
         return comment
 
     def _get_tag_html(self, namelist, tag, val, comment_indents, file_id):
-        package = self.package[file_id]
+        package = self.packages[file_id]
         database = self._database[package][namelist]
         if self.code == "qe":
             link = f"{package}.html#" + database[tag]["id"]
@@ -265,10 +283,27 @@ class Codex:
         return textwrap.indent(tag_link, " " * INDENT)
 
     def _get_preview_html(self, tag, namelist, file_id):
-        p = self.package[file_id]
+        p = self.packages[file_id]
         nl = namelist
         preview_link = os.path.join(f"tags-{self.code}", p, nl, f"{tag}.html")
         preview = f'<div class="preview" id="preview_{tag}" data-fileid="{file_id}">\n'
         preview += f'<object data="{preview_link}" class="preview-object" type="text/html">'
         preview += " </object>\n</div>\n"
         return textwrap.indent(preview, " " * INDENT)
+
+    @staticmethod
+    def _flatten_dict(d):
+        """
+        Takes a dict of dicts of dicts and flattens it into a dict of lists
+        e.g.,
+        {'a': {'b': {'c': 1, 'd': 2}, 'e': {'f': 3, 'g': 4}}}
+        becomes
+        {'a': ['b.c', 'b.d', 'e.f', 'e.g']}
+        """
+        flat = {}
+        for top_key, middle_dict in d.items():
+            flat[top_key] = []
+            for middle_key, bottom_dict in middle_dict.items():
+                for bottom_key in bottom_dict.keys():
+                    flat[top_key].append(middle_key + "." + bottom_key)
+        return flat
