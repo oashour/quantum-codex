440c9329e0ee1d230cccaecc4340b3c877befd82
Omar Ashour <omar.ashour22@gmail.com>
2023-06-12 01:55:12 -0700

    Basic file selection page

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
-from codex import Codex
-import f90nml
+"""
+Main CODEX flask web app
+"""
+
 import os
 from base64 import b64decode
 import json
 from importlib import resources
 import shutil
 
-def generate_tag_webpages(dir='static', dbversion='7.2'):
+from flask import Flask, flash, request, redirect, url_for, render_template
+from flask_uploads import IMAGES, UploadSet, configure_uploads
+from werkzeug.utils import secure_filename
+import f90nml
+
+from codex import Codex
+
+
+def generate_tag_webpages(dir="static", dbversion="7.2"):
     base_db_dir = resources.files("codex.database")
     code = "qe"  # Need better terminology
     database_dir = os.path.join(base_db_dir, f"{code}-{dbversion}")
@@ -26,20 +35,44 @@ def generate_tag_webpages(dir='static', dbversion='7.2'):
         for nl, namelist in package.items():
             os.mkdir(os.path.join(tags_dir, p, nl))
             for tag, v in namelist.items():
-                if v["html"] != "":#'No documentation was found for this tag.':
+                if v["html"] != "":  #'No documentation was found for this tag.':
                     webpage = b64decode(v["html"]).decode("utf-8")
                     path = os.path.join(tags_dir, p, nl, f"{tag}.html")
                     with open(path, "w") as f:
                         f.write(webpage)
 
+
 app = Flask(__name__)
+inputs = UploadSet("inputs", ['pwi', 'in', 'vasp'])
+app.config["UPLOADED_INPUTS_DEST"] = "static/input_files"
+app.config["SECRET_KEY"] = os.urandom(24)
+configure_uploads(app, inputs)
+
+@app.route("/", methods=["GET", "POST"])
+def upload():
+    #if request.method == "GET":
+    #    file_name = "example/sr3pbo_bands.in"
+    #    dbversion = 7.2
+    #    codex = Codex(file_name, dbversion)
+    #    generate_tag_webpages(dbversion=dbversion)
+
+    #    return render_template("input_file.html.j2", codex=codex, indent=" " * 2)
+    if request.method == 'POST'and 'input_file' in request.files:
+        print(request.files)
+        print(request.form)
+
+        input_file = request.files['input_file']
+        print(input_file.filename)
+        inputs.save(input_file)
+        dbversion = request.form['dbversion'].split('latest')[0].strip('() ')
+        print(dbversion)
+        code = request.form['code']
+        file_name = inputs.path(input_file.filename)
+        codex = Codex(file_name, dbversion)
+        flash("Input File saved successfully.")
+        generate_tag_webpages(dbversion=dbversion)
+
+        return render_template("input_file.html.j2", codex=codex, indent=" " * 2)
+    return render_template('upload.html.j2')
 
 
-@app.route("/")
-def hello_world():
-    file_name = "example/sr3pbo_bands.in"
-    dbversion = 7.2
-    codex = Codex(file_name, dbversion)
-    generate_tag_webpages(dbversion=dbversion)
-
-    return render_template("input_file.html.j2", codex=codex, indent=" " * 2)
