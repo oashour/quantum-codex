4a3150908186f5a8ef0137cab45407202ebfa3b3
Omar Ashour <omar.ashour22@gmail.com>
2023-06-28 04:32:56 -0700

    Add support for VASP files without an extension

--- codex/app/extensions.py
+++ codex/app/extensions.py
@@ -1,7 +1,25 @@
+"""
+Flask extensions used by Codex
+"""
+import re
+from werkzeug.datastructures import FileStorage
 from flask_uploads import UploadSet
+from flask_uploads.extensions import extension
 from flask_pymongo import PyMongo
 from flask_smorest import Api
 
-inputs = UploadSet("inputs", ["pwi", "in", "vasp"])
+
+class CodexUploadSet(UploadSet):
+    """
+    Custom UploadSet for Codex, checks the extension of the file or the start of the file name
+    """
+
+    def file_allowed(self, storage: FileStorage, basename: str) -> bool:
+        return re.match(
+            r"^(INCAR|POSCAR|KPOINTS|POTCAR)", storage.filename
+        ) or self.extension_allowed(extension(basename))
+
+
+inputs = CodexUploadSet("inputs", ["pwi", "in", "vasp"])
 mongo = PyMongo()
 api = Api()
